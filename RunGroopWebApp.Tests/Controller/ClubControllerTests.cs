6a5c443e90a3f05292834134371a0b0b161a608d
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-07-22 13:49:18 +0300

    Tested index controller in ClubController

--- RunGroopWebApp.Tests/Controller/ClubControllerTests.cs
+++ RunGroopWebApp.Tests/Controller/ClubControllerTests.cs
@@ -1,62 +1,71 @@
-﻿using FakeItEasy;
-using FluentAssertions;
-using Microsoft.AspNetCore.Http;
+﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using RunGroopWebApp.Controllers;
 using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
-using RunGroopWebApp.Services;
+using RunGroopWebApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
-
 namespace RunGroopWebApp.Tests.Controller
 {
     public class ClubControllerTests
     {
-        private ClubController _clubController;
-        private IClubRepository _clubRepository;
-        private IPhotoService _photoService;
-        private IHttpContextAccessor _httpContextAccessor;
+        private readonly Mock<IClubRepository> _clubRepository;
+        private readonly Mock<IPhotoService> _photoService;
+        private readonly ClubController _clubController;
         public ClubControllerTests()
         {
             //Dependencies
-            _clubRepository = A.Fake<IClubRepository>();
-            _photoService = A.Fake<IPhotoService>();
-            _httpContextAccessor = A.Fake<HttpContextAccessor>();
-
-            //SUT
-            _clubController = new ClubController(_clubRepository, _photoService);
+            _clubRepository = new Mock<IClubRepository>();
+            _photoService = new Mock<IPhotoService>();
+            //SUT --> System under test
+            _clubController = new ClubController(_clubRepository.Object, _photoService.Object);
         }
 
         [Fact]
-        public void ClubController_Index_ReturnsSuccess()
+        public async Task ClubController_Index_ReturnsViewResult_WithExpectedModel()
         {
             //Arrange - What do i need to bring in?
-            var clubs = A.Fake<IEnumerable<Club>>();
-            A.CallTo(() => _clubRepository.GetAll()).Returns(clubs);
+            var clubs = new List<Club>
+            {
+                new Club { Id = 1, Title = "Test Club A" },
+                new Club { Id = 2, Title = "Test Club B" }
+            };
+
+            _clubRepository.Setup(r => r.GetSliceAsync(0, 6)).ReturnsAsync(clubs);
+            _clubRepository.Setup(r => r.GetCountAsync()).ReturnsAsync(clubs.Count);
+
+
             //Act
-            var result = _clubController.Index();
-            //Assert - Object check actions
-            result.Should().BeOfType<Task<IActionResult>>();
+            var result = await _clubController.Index();
+
+            //Assert - Object check actions and view model
+            result.Should().BeOfType<ViewResult>();
+            var viewResult = result as ViewResult;
+            viewResult!.Model.Should().BeOfType<IndexClubViewModel>();
+            var model = viewResult.Model as IndexClubViewModel;
+
+            model!.Clubs.Should().HaveCount(2);
+            model.Clubs.First().Title.Should().Be("Test Club A");
+            model.Page.Should().Be(1);
+            model.PageSize.Should().Be(6);
+            model.TotalClubs.Should().Be(2);
+            model.TotalPages.Should().Be(1);
+            model.Category.Should().Be(-1);
         }
 
         [Fact]
-        public void ClubController_Detail_ReturnsSuccess()
+        public async Task ClubController_Index_ReturnsNotFound_WhenInvalidPageOrPageSize()
         {
-            //Arrange
-            var id = 1;
-            var club = A.Fake<Club>();
-            A.CallTo(() => _clubRepository.GetByIdAsync(id)).Returns(club);
-            //Act
-            var result = _clubController.DetailClub(id, "RunningClub");
+            // Act
+            var result = await _clubController.Index(page: 0, pageSize: 0);
+
             //Assert
-            result.Should().BeOfType<Task<IActionResult>>();
+            result.Should().BeOfType<NotFoundResult>();
         }
-
-
     }
 }
