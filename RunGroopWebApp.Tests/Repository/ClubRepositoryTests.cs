34d5fb207ca9c4d21c3044f4a23b6d2c1ef368a0
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-07-22 20:36:09 +0300

    Create ClubRepositoryTests.cs

---
+++ RunGroopWebApp.Tests/Repository/ClubRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunGroopWebApp.Tests.Repository
+{
+    public class ClubRepositoryTests
+    {
+        private async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>
+        }
+    }
+}
