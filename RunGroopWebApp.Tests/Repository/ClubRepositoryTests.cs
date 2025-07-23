7e2f297d83508208374e736a3853bba7597e6a24
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-07-23 22:05:48 +0300

    some tests on ClubRepository/GetByIdAsync

--- RunGroopWebApp.Tests/Repository/ClubRepositoryTests.cs
+++ RunGroopWebApp.Tests/Repository/ClubRepositoryTests.cs
@@ -9,11 +9,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
 
 namespace RunGroopWebApp.Tests.Repository
 {
     public class ClubRepositoryTests
     {
+        private readonly ClubRepository _repository;
+
         private async Task<ApplicationDbContext> GetDbContext()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -21,16 +26,15 @@ namespace RunGroopWebApp.Tests.Repository
                 .Options;
             var databaseContext = new ApplicationDbContext(options);
             databaseContext.Database.EnsureCreated();
-            if (await databaseContext.Clubs.CountAsync() < 0)
+            if (await databaseContext.Clubs.CountAsync() <= 0)
             {
-                for (int i = 0; i < 10; i++)
+                var testClubs = new[]
                 {
-                    databaseContext.Clubs.Add(
                     new Club()
                     {
                         Title = "Running Club 1",
                         Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
-                        Description = "This is the description of the first cinema",
+                        Description = "This is the description of the first club",
                         ClubCategory = ClubCategory.City,
                         Address = new Address()
                         {
@@ -38,12 +42,49 @@ namespace RunGroopWebApp.Tests.Repository
                             City = "Charlotte",
                             State = "NC"
                         }
-                    });
-                    await databaseContext.SaveChangesAsync();
-                }
+                    },
+                    new Club()
+                    {
+                        Title = "Trail Running Club",
+                        Image = "https://example.com/trail.jpg",
+                        Description = "Trail running enthusiasts",
+                        ClubCategory = ClubCategory.Trail,
+                        Address = new Address()
+                        {
+                            Street = "456 Trail Rd",
+                            City = "Asheville",
+                            State = "NC"
+                        }
+                    },
+                    new Club()
+                    {
+                        Title = "Endurance Running Club",
+                        Image = "https://example.com/endurance.jpg",
+                        Description = "Long distance endurance running",
+                        ClubCategory = ClubCategory.Endurance,
+                        Address = new Address()
+                        {
+                            Street = "789 Speed Ave",
+                            City = "Raleigh",
+                            State = "NC"
+                        }
+                    },
+                    new Club()
+                    {
+                        Title = "Club Without Address",
+                        Image = "https://example.com/no-address.jpg",
+                        Description = "Club with no physical address",
+                        ClubCategory = ClubCategory.City,
+                        Address = null
+                    }
+                };
+
+                await databaseContext.Clubs.AddRangeAsync(testClubs);
+                await databaseContext.SaveChangesAsync();
             }
             return databaseContext;
         }
+
 
         [Fact]
         public async void ClubRepository_Add_ReturnsBool()
@@ -71,6 +112,63 @@ namespace RunGroopWebApp.Tests.Repository
             //Assert
             result.Should().BeTrue();
 
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_WhenClubExists_ShouldReturnClubWithAddress()
+        {
+            //Arrange
+            using var context = await GetDbContext();
+            var repository = new ClubRepository(context);
+            //Act
+            var expectedClub = await context.Clubs.Include(c => c.Address).FirstAsync();
+            var expectedId = expectedClub.Id;
+            //Assert
+            var result = await repository.GetByIdAsync(expectedId);
+
+            result.Should().NotBeNull();
+            result.Id.Should().Be(expectedId);
+            result.Title.Should().Be("Running Club 1");
+            result.Description.Should().Be("This is the description of the first club");
+            result.ClubCategory.Should().Be(ClubCategory.City);
+            result.Address.Should().NotBeNull();
+            result.Address.Street.Should().Be("123 Main St");
+            result.Address.City.Should().Be("Charlotte");
+            result.Address.State.Should().Be("NC");
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_WhenClubDoesNotExist_ShouldReturnNull()
+        {
+            //Arrange
+            using var context = await GetDbContext();
+            var repository = new ClubRepository(context);
+            var nonExistenId = 9999;
+
+            //Act
+            var result = await repository.GetByIdAsync(nonExistenId);
+
+            //Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_WhenClubExistsWithoutAddress_ShouldReturnClubWithNullAddress()
+        {
+            //Arrange
+            using var context = await GetDbContext();
+            var repository = new ClubRepository(context);
+
+            //Act
+            var clubWithoutAddress = await context.Clubs
+                .Where(c => c.Address == null)
+                .FirstAsync();
+            var result = await repository.GetByIdAsync(clubWithoutAddress.Id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Address.Should().BeNull();
+            result.Title.Should().Be("Club Without Address");
         }
     }
 }
